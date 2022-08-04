42c56e243e3830611b10526ea5d961608e76f44f
Seveilith <johansson.joakim@me.com>
2022-08-04 11:23:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-d7cxo4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-d7cxo4)
---
+++ src/app/tooltip-basic.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-tooltip-basic',
+  templateUrl: './tooltip-basic.html',
+  host: { 'class': 'd-block' }
+})
+export class NgbdTooltipBasic {
+}
